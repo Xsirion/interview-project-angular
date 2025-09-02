213288af9f77ed0f6ec0221517985c9067780988
Pawel Grzybek <xsirion@gmail.com>
2025-09-02 12:46:22 +0200

    init eslint prettierand tailwindcss

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      'prettier': prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
